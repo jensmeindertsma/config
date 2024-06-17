fe4629c508ea91ce836ba9689a2ab7c7c4af5c73
Jens Meindertsma <mail@jensmeindertsma.com>
2024-06-17 16:41:33 +0200

    push stuff together

--- machines/badger/configuration.nix
+++ machines/badger/configuration.nix
@@ -4,11 +4,8 @@
   ...
 }: {
   system.stateVersion = 4;
-
   services.nix-daemon.enable = true;
-
   nix.settings.experimental-features = "nix-command flakes";
-
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfree = true;
 
