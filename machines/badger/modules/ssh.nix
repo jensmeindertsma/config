6541781430f33b75ef4142d716113db35c459150
Jens Meindertsma <mail@jensmeindertsma.com>
2024-06-17 17:07:24 +0200

    ssh stuff

--- machines/badger/modules/ssh.nix
+++ machines/badger/modules/ssh.nix
@@ -1,3 +1,19 @@
 {...}: {
-  # TODO
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host github.com
+        IdentityFile    ~/.ssh/id_ed25519
+        AddKeysToAgent  yes
+        UseKeychain     yes
+
+      Host ubuntu
+        HostName        127.0.0.1
+        Port            2001
+        User            jens
+        ForwardAgent    yes
+        AddKeysToAgent  yes
+        UseKeychain     yes
+    '';
+  };
 }
