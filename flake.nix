d7c3925aeb5f5538055a9ab13848953e15acaa48
Jens Meindertsma <mail@jensmeindertsma.com>
2024-06-19 21:57:44 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,27 +2,23 @@
   description = "A single (snow)flake that produces all my configuration for all my devices";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
 
     nix-darwin = {
       url = "github:lnl7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
   outputs = inputs @ {
     self,
-    nixpkgs,
+    nixpkgs-darwin,
     nix-darwin,
     home-manager,
-    nix-vscode-extensions
+    nix-vscode-extensions,
   }: {
     darwinConfigurations.badger = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
