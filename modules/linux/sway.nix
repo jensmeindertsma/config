caa3b9eb2d6a4bd216eb75457164a565f3c94ada
Jens Meindertsma <mail@jensmeindertsma.com>
2024-07-05 14:13:02 +0200

    left right workspace

--- modules/linux/sway.nix
+++ modules/linux/sway.nix
@@ -62,6 +62,8 @@
           "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'";
           "XF86MonBrightnessDown" = "exec 'brightnessctl set 5%-'";
           "XF86MonBrightnessUp" = "exec 'brightnessctl set +5%'";
+          "${modifier}+Ctrl+Left" = "workspace prev";
+          "${modifier}+Ctrl+Right" = "workspace next";
         };
       bars = [
         {
