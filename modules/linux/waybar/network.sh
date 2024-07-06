1d3bdfd7571c4a976942845b47ea3af23fbb5a23
Jens Meindertsma <mail@jensmeindertsma.com>
2024-07-06 11:49:29 +0200

    ethernet in network script

--- modules/linux/waybar/network.sh
+++ modules/linux/waybar/network.sh
@@ -4,34 +4,42 @@
 
 INFO=$(nmcli)
 
-IS_DISCONNECTED=$(echo -e "$INFO" | grep -v "p2p-dev" | grep disconnected)
-
-if [[ -n "$IS_DISCONNECTED" ]]; then
-	printf '{"text": "󰖪 disconnected!", "tooltip": ""}\n'
-	exit
-fi
-
+IP=$(echo -e "$INFO" | grep "ip4 default" -A 1 | grep "inet4" | cut -f 2 -d ' ' | cut -f 1 -d '/')
 IS_VPN=$(echo -e "$INFO" | grep "VPN connection")
+IS_WIRED=$(echo -e "$INFO" | grep 'ethernet (' -B 2 | grep "connected to")
+WIFI_NAME=$(nmcli -t -f active,ssid dev wifi list --rescan no | grep '^yes' | cut -d: -f2)
+
 
 if [[ -n "$IS_VPN" ]]; then
 	VPN_NAME=$(echo -e "$INFO" | grep "VPN connection" | cut -f 1 -d ' ')
-	IP=$(echo -e "$INFO" | grep "ip4 default" -A 1 | grep "inet4" | cut -f 2 -d ' ' | cut -f 1 -d '/')
-	WIFI_NAME=$(nmcli -t -f active,ssid dev wifi list --rescan no | grep '^yes' | cut -d: -f2)
-	printf "{\"text\": \"󰦝 $IP ($VPN_NAME)\", \"tooltip\": \"󰖩  connected to '$WIFI_NAME'\"}\n"
+	if [[ -n "$IS_WIRED" ]]; then
+		SUBSTRING="connected to "
+		NET_NAME="${IS_WIRED#*$SUBSTRING}"
+		printf "{\"text\": \"󰦝 $IP ($VPN_NAME)\", \"tooltip\": \"󰈀  connected to '$NET_NAME'\"}\n"
+	else 
+		printf "{\"text\": \"󰦝 $IP ($VPN_NAME)\", \"tooltip\": \"󰖩  connected to '$WIFI_NAME'\"}\n"
+	fi
 	exit
 fi
 
 IS_WIFI=$(echo -e "$INFO" | grep "wifi " -B 2 | grep "connected to")
 
 if [[ -n "$IS_WIFI" ]]; then
-	IP=$(echo -e "$INFO" | grep "ip4 default" -A 1 | grep "inet4" | cut -f 2 -d ' ' | cut -f 1 -d '/')
-	WIFI_NAME=$(nmcli -t -f active,ssid dev wifi list --rescan no | grep '^yes' | cut -d: -f2)
 	printf "{\"text\": \"󰖩  $IP ($WIFI_NAME)\", \"tooltip\": \"\"}\n"
 	exit
 fi
 
-# TODO: wired connection
+if [[ -n "$IS_WIRED" ]]; then
+	printf "{\"text\": \"󰈀  $IP\", \"tooltip\": \"\"}\n"
+	exit
+fi
 
+IS_DISCONNECTED=$(echo -e "$INFO" | grep -v "p2p-dev" | grep disconnected)
+
+if [[ -n "$IS_DISCONNECTED" ]]; then
+	printf '{"text": "󰖪 disconnected!", "tooltip": ""}\n'
+	exit
+fi
 
 # DISABLED
 IS_DISABLED=$(echo -e "INFO" | grep "wifi " -B 2 | grep "sw disabled")
