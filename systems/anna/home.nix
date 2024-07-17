c18708736cdd8e0b477cdde0a93deda5b847a38e
Jens Meindertsma <mail@jensmeindertsma.com>
2024-07-17 15:23:59 +0200

    add SSH hosts

--- systems/anna/home.nix
+++ systems/anna/home.nix
@@ -15,6 +15,31 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    matchBlocks = {};
+    matchBlocks = {
+      "i:ubuntu" = {
+        hostname = "192.168.101.240";
+        user = "jens";
+      };
+      "i:fedora" = {
+        hostname = "192.168.101.199";
+        user = "jens";
+      };
+      "l:ubuntu" = {
+        hostname = "192.168.100.192";
+        user = "jens";
+      };
+      "l:fedora" = {
+        hostname = "192.168.100.217";
+        user = "jens";
+      };
+      "l:opensuse" = {
+        hostname = "192.168.100.226";
+        user = "jens";
+      };
+      "l:rocky" = {
+        hostname = "192.168.100.130";
+        user = "jens";
+      };
+    };
   };
 }
