532706718bc1e476b7e366ac54263e661b2d5ecd
Jens Meindertsma <mail@jensmeindertsma.com>
2024-09-15 20:31:59 +0200

    stuff

---
+++ tools/install-vscode-mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing Visual Studio Code settings..."
+
+ln -sf ~/Users/Jens/Projects/jensmeindertsma/config/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
