ba37ab6f57ef29e232e1221af774e837099c4f03
Jens Meindertsma <mail@jensmeindertsma.com>
2025-11-01 14:59:58 +0100

    Waybar on Wyvern

--- systems/wyvern/.config/waybar/modules/date.py
+++ systems/wyvern/.config/waybar/modules/date.py
@@ -4,9 +4,10 @@ import json
 now = datetime.now()
 
 date = now.strftime("%d/%m/%Y").lower()
+day = now.strftime("%A").lower()
 
 output = {
-    "text": f'{date}',
+    "text": f'{date} ({day})',
 }
 
 print(json.dumps(output))
