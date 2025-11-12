e5e92c06c3c0a1260d576b5785d211c6a163d0be
Jens Meindertsma <mail@jensmeindertsma.com>
2025-11-12 17:24:49 +0100

    start

--- systems/anna/.config/fuzzel/launch.sh
+++ systems/anna/.config/fuzzel/launch.sh
@@ -4,7 +4,7 @@
 THEME=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$THEME" == "'prefer-dark'" ]]; then
-    fuzzel --config ~/.config/fuzzel/dark.ini
+    fuzzel --launch-prefix="uwsm app --" --config ~/.config/fuzzel/dark.ini
 else
-    fuzzel --config ~/.config/fuzzel/light.ini
+    fuzzel --launch-prefix="uwsm app --" --config ~/.config/fuzzel/light.ini
 fi
