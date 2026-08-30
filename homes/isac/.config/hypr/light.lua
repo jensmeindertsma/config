0cf47dc9f358bf4110b2be18c9911410965cbd86
Jens Meindertsma <mail@jensmeindertsma.com>
2026-08-30 13:45:13 +0200

    Add `homes/isac` (#2)

---
+++ homes/isac/.config/hypr/light.lua
@@ -0,0 +1,12 @@
+hl.config({
+	general = {
+		border_size = 6,
+		gaps_in = -3,
+		gaps_out = 0,
+		col = {
+			active_border = "#FFFFFF",
+			inactive_border = "#FFFFFF",
+			-- active_border = "#FF6A13",
+		},
+	},
+})
