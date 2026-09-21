7c73b680fb89620fb90e2a91bb4b9e19375f94bb
Jens Meindertsma <mail@jensmeindertsma.com>
2026-09-21 11:32:00 +0200

    Reorganize configuration files

--- homes/isac/.config/waybar/date.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-now = datetime.now()
-
-print(f"{now:%B} {now:%-d} ({now:%A})".lower())
--- homes/striker/.config/hypr/dark.lua
+++
@@ -1,8 +0,0 @@
-hl.config({
-	general = {
-		col = {
-			active_border = "rgba(333333ff)",
-			inactive_border = "rgba(333333ff)",
-		},
-	},
-})
